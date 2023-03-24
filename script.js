bd782eb07d474575a6c8b30239f7eb58ba27e9e6
ankitxcoding <ankitxcoding@gmail.com>
2023-03-24 11:14:03 +0530

    bus tracking website

---
+++ script.js
@@ -0,0 +1,20 @@
+function populate(s1,s2){
+	var s1 = document.getElementById(s1);
+	var s2 = document.getElementById(s2);
+	s2.innerHTML = "";
+	if(s1.value == "jaunpur")
+	{
+		var optionArray = ["|--to--","shahganj|Shahganj"];
+	} else if(s1.value == "shahganj")
+	{
+		var optionArray = ["|--to--","jaunpur|Jaunpur"];
+	} 
+	for(var option in optionArray)
+	{
+		var pair = optionArray[option].split("|");
+		var newOption = document.createElement("option");
+		newOption.value = pair[0];
+		newOption.innerHTML = pair[1];
+		s2.options.add(newOption);
+    }
+}
